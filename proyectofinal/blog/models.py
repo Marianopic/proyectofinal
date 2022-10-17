0734dcdc4741b54d4ee503553e7d9ffe8d3696c0
Marianopic <46465080+Marianopic@users.noreply.github.com>
2022-10-17 19:21:24 -0300

    corrijo charfield

--- proyectofinal/blog/models.py
+++ proyectofinal/blog/models.py
@@ -10,10 +10,15 @@ class Autor(models.Model):
         
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
-    profesion = models.EmailField(max_length=30)
+    profesion = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.nombre
 
 
 class Articulo(models.Model):
+    
+        
     titulo = models.CharField(max_length=30)
     texto = models.CharField(max_length=1000)
     fecha = models.DateField(null=True)
